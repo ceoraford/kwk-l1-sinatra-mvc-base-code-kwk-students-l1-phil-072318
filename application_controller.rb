61a3ebc89755cf4d63817caf0c3a7b361b1c185b
Ceora Ford <ceoraallthetime@gmail.com>
2018-08-03 16:16:42 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,22 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
+  get '/' do 
+    "This is our home page"
+  end 
+  get '/AllAboutMe' do 
+    erb:index
   end
-
+  get '/hello/:name' do 
+    @yourname = params[:name]
+    "Hello #{@yourname}"
+  end 
+  get  '/favoriteanimal' do 
+    erb:favorite_animal
+  end 
+  post '/favoriteanimal' do 
+    "My name is #{params[:name]} and I love #{params[:favoriteanimal]}s"
+  end
 end
